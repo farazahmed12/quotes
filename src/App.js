e2c1cfadd0c6b97b48ba63f4ce9e5f4fa7865dfa
farazahmed-fa <masterdot707@gmail.com>
2021-12-19 09:12:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchAdvice = () => {
+      axios
+        .get("https://api.adviceslip.com/advice")
+        .then((res) => {
+          setAdvice(res.data.slip.advice);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="intro" className="bg-image shadow-2-strong">
+      <div>
+        <div className="container  align-items-center justify-content-center text-center h-100">
+          <div className="text-white">
+            <h1 className="pt-5">{advice}</h1>
+
+            <button type="button" className="btn btn-dark btn-rounded">
+              Check New Quote
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
